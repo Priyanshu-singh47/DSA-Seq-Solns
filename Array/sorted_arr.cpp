0de03ecf5a66dde174d0392a6f9cf37d273c2cc5
Priyanshu-singh47 <dispriyanshu47@gmail.com>
2026-06-09 15:08:08 +0530

    added program to check sorted arr or not

---
+++ Array/sorted_arr.cpp
@@ -0,0 +1,15 @@
+class Solution{	
+	public:
+		bool isSorted(vector<int>& nums){
+			 
+        for(int i=0 ; i<nums.size()-1 ; i++){
+            if( nums[i+1]>=nums[i]){
+        
+            }
+            else return false ;  
+    
+        }
+       return true ; 
+    }
+};
+		
