c364bddb9f2bc4ad1a39e07971a80f4755efd6be
Priyanshu-singh47 <dispriyanshu47@gmail.com>
2026-06-12 15:43:29 +0530

    Rearrange ele by sign

---
+++ Array/rearrangeElebysize.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        vector<int>ans(nums.size());
+        int posi=0;int negi=1;
+        for(int i=0;i<nums.size();i++){
+            if (nums[i]>0){
+                ans[posi]=nums[i];
+                posi+=2;
+            
+            }
+            else {
+            ans[negi]=nums[i];
+            negi+=2;
+            }
+        }
+        return ans;
+    }
+};
