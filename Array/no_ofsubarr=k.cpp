1ca853a8191f24cb41a5c3867396e946df4b2c54
Priyanshu-singh47 <dispriyanshu47@gmail.com>
2026-06-14 11:41:34 +0530

    hi

---
+++ Array/no_ofsubarr=k.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int presum=0;int count=0;
+    
+        unordered_map<int,int>mpp;
+        mpp[0]=1;
+        for(int i=0;i<nums.size();i++){
+            presum+=nums[i];
+            int remove=presum-k;
+            count+=mpp[remove];
+            mpp[presum]+=1;        
+        }
+        return count;
+    }
+};
