9e261388da09dbc752a5cb08030370fea9ab938c
Priyanshu-singh47 <dispriyanshu47@gmail.com>
2026-06-18 10:54:47 +0530

    Two more added

---
+++ Min in Rot_arr.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+         int low=0;int high=nums.size()-1;int mini=INT_MAX;
+        while(low<=high){
+            if(nums[low]<=nums[high]){
+                mini=min(mini,nums[low]);
+                break;
+            
+            }
+            int mid=(low+high)/2;
+            if(nums[mid]>=nums[low]){
+                mini=min(mini,nums[low]);
+                low=mid+1;
+                }               
+            else{
+                mini=min(mini,nums[mid]);
+                high=mid-1;
+              
+                }
+                
+            
+        }
+        return mini;
+        
+    }
+};
