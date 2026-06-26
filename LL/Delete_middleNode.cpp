71545090c146a2874aedc99d31b00130fda2d525
Priyanshu-singh47 <dispriyanshu47@gmail.com>
2026-06-26 10:40:33 +0530

    added

---
+++ LL/Delete_middleNode.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+
+        if (head == nullptr || head->next == nullptr)
+            return nullptr;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        fast = head->next->next;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* delNode = slow->next;
+        slow->next = slow->next->next;
+        delete delNode;
+
+        return head;
+    }
+};
