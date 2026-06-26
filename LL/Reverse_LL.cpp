b8d9dcfdfca410b88b6b808510b35b18256124d3
Priyanshu-singh47 <dispriyanshu47@gmail.com>
2026-06-26 07:01:23 +0530

    added

--- LL/Reverse_LL.cpp
+++ LL/Reverse_LL.cpp
@@ -13,3 +13,47 @@ public:
         return prev;
     }
 };
+ 
+//optimal below
+
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+     ListNode* prev=nullptr;
+     ListNode* temp=head;
+     while(temp!=nullptr){
+        ListNode* front=temp->next;
+        temp->next=prev;
+        prev=temp;
+        temp=front;
+
+     }   
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast->next!=nullptr && fast->next->next!=nullptr){
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+          ListNode* newhead=reverseList(slow->next);
+          ListNode* first=head;
+            ListNode* second=newhead;
+            while(second!=nullptr){
+                if(first->val!=second->val){
+                    reverseList(newhead);
+                    return false;
+                }
+                first=first->next;
+                second=second->next;
+            }
+            reverseList(newhead);
+            return true;
+
+        
+        
+    }
+};
