d91f60ae71ce4f793b53c529571eed6307728bbd
Priyanshu-singh47 <dispriyanshu47@gmail.com>
2026-06-28 11:13:00 +0530

    added

---
+++ stackqueue/implnt q using s.cpp
@@ -0,0 +1,58 @@
+class MyQueue {
+public:
+    stack<int>s1;
+    stack<int>s2;
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        s1.push(x);
+        
+    }
+    
+    int pop() {
+        if(s2.empty()!=1) {
+            int val=s2.top();
+            s2.pop();
+            return val;
+        }
+         else{
+            while(s1.size()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+         }
+         int val=s2.top();
+         s2.pop();
+         return val;
+        
+    }
+    
+    int peek() {
+         if(s2.empty()!=1) return s2.top();
+         else{
+            while(s1.size()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+         }
+         return s2.top();
+        
+    }
+    
+    bool empty() {
+        if(s1.empty() && s2.empty()) return true;
+        else return false;
+        
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
