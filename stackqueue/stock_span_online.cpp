5483907f27e3c18ee12a9031107a2cf1eea31801
Priyanshu-singh47 <dispriyanshu47@gmail.com>
2026-07-02 10:52:05 +0530

    added

---
+++ stackqueue/stock_span_online.cpp
@@ -0,0 +1,24 @@
+class StockSpanner {
+public:
+    stack<pair<int,int>>st;
+    int ind=-1;int ans=0;
+    StockSpanner() {
+         ind=-1;
+        
+    }
+    
+    int next(int price) {
+        ind++;
+        while(!st.empty() && price>=st.top().first) st.pop();
+        ans=ind-(st.empty() ? -1 : st.top().second);
+        st.push({price,ind});
+        return ans;
+        
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
