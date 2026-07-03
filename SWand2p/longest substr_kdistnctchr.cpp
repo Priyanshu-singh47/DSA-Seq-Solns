7c33847ba718f6f02e5eeb560d6ab5b5924e6e77
Priyanshu-singh47 <dispriyanshu47@gmail.com>
2026-07-03 17:25:07 +0530

    added

---
+++ SWand2p/longest substr_kdistnctchr.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int kDistinctChar(string& s, int k) {
+       int n=s.length();
+       int l=0;int r=0; int maxlen=0;unordered_map<char,int>mpp;
+       while(r<n){
+          mpp[s[r]]++;
+          if(mpp.size()>k){
+            mpp[s[l]]--;
+            if(mpp[s[l]]==0) mpp.erase(s[l]);
+            l++;
+            }
+          if(mpp.size()<=k) maxlen=max(maxlen,r-l+1);
+          r++;
+       }
+       return maxlen;
+    }
+};
