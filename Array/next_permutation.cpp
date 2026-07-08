1f5e71e8b7b1fd17b00e118b1d90bd6e695f9866
Priyanshu-singh47 <dispriyanshu47@gmail.com>
2026-07-08 13:12:59 +0530

    array q upd

--- Array/next_permutation.cpp
+++ Array/next_permutation.cpp
@@ -21,3 +21,36 @@ public:
         reverse(nums.begin()+index+1,nums.end());
     }
 };
+
+//brute
+
+class Solution {
+public:
+    void solve(vector<int>& nums, int idx, vector<vector<int>>& ans) {
+        if (idx == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+
+        for (int i = idx; i < nums.size(); i++) {
+            swap(nums[idx], nums[i]);
+            solve(nums, idx + 1, ans);
+            swap(nums[idx], nums[i]); // backtrack
+        }
+    }
+
+    void nextPermutation(vector<int>& nums) {
+        vector<vector<int>> perms;
+
+        solve(nums, 0, perms);
+
+        sort(perms.begin(), perms.end());
+
+        for (int i = 0; i < perms.size(); i++) {
+            if (perms[i] == nums) {
+                nums = perms[(i + 1) % perms.size()];
+                return;
+            }
+        }
+    }
+};
