def3126d477ec26b900f948f71ef300d28b9a01f
Priyanshu-singh47 <dispriyanshu47@gmail.com>
2026-07-14 19:03:38 +0530

    Pre-post-in in 1Trav

---
+++ trees/Prepostinorder1trav.cpp
@@ -0,0 +1,58 @@
+class Solution{
+public:
+
+    vector<vector<int>> treeTraversal(TreeNode* root){
+
+        vector<vector<int>> ans;
+
+        if(root == NULL)
+            return ans;
+
+        vector<int> pre, inorder, post;
+
+        stack<pair<TreeNode*, int>> st;
+
+        st.push({root, 1});
+
+        while(!st.empty()){
+
+            auto it = st.top();
+            st.pop();
+
+            if(it.second == 1){
+
+                pre.push_back(it.first->data);
+
+                it.second++;
+
+                st.push(it);
+
+                if(it.first->left)
+                    st.push({it.first->left, 1});
+            }
+
+            else if(it.second == 2){
+
+                inorder.push_back(it.first->data);
+
+                it.second++;
+
+                st.push(it);
+
+                if(it.first->right)
+                    st.push({it.first->right, 1});
+            }
+
+            else{
+
+                post.push_back(it.first->data);
+            }
+        }
+
+        ans.push_back(inorder);
+        ans.push_back(pre);
+        ans.push_back(post);
+
+        return ans;
+    }
+};
